cbf0b43b941b1be5f8f0f8bb595e00b97448bd82
matheus <matheus23molina@gmail.com>
2025-11-24 00:10:56 -0300

    Aula 3 completa

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let cardsContainer = document.querySelector(".card-container");
+let campoBusca = document.querySelector("#campo-busca");
 let dados = [];
 
 async function iniciarBusca() {
-    let resposta = await fetch("data.json");
-    dados = await resposta.json();
-    renderizarCards(dados);
+    // Se os dados ainda não foram carregados, busca do JSON.
+    if (dados.length === 0) {
+        let resposta = await fetch("data.json");
+        dados = await resposta.json();
+    }
+
+    const termoBusca = campoBusca.value.toLowerCase();
+
+    const dadosFiltrados = dados.filter(dado => {
+        // Busca no nome e na descrição da linguagem
+        return dado.nome.toLowerCase().includes(termoBusca) ||
+               dado.descricao.toLowerCase().includes(termoBusca);
+    });
+
+    renderizarCards(dadosFiltrados);
 }
 
 function renderizarCards(dados) {
+    cardsContainer.innerHTML = ""; // Limpa os cards antigos antes de renderizar os novos
     for (let dado of dados) {
         let article = document.createElement("article");
         article.classList.add("card");
