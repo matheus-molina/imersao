2f562795a9761c4e0306dfcd0f39d9c3aaff6491
matheus <matheus23molina@gmail.com>
2025-11-24 00:01:38 -0300

    Metade da terceira aula

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+let cardsContainer = document.querySelector(".card-container");
 let dados = [];
 
 async function iniciarBusca() {
     let resposta = await fetch("data.json");
     dados = await resposta.json();
-    console.log(dados);
+    renderizarCards(dados);
+}
+
+function renderizarCards(dados) {
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p>${dado.ano}</p>
+        <p>${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `
+        cardsContainer.appendChild(article);
+    }
 }
